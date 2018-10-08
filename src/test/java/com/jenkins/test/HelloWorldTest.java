da4e986d824102643cad2e3c019e67ef065e4a24
anandpanneer <anand.panneer@gmail.com>
2018-10-08 13:07:42 +0530

    Refactoring test file

--- src/test/java/com/jenkins/test/HelloWorldTest.java
+++ src/test/java/com/jenkins/test/HelloWorldTest.java
@@ -1,21 +1,26 @@
 package com.jenkins.test;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class HelloWorldTest {
+    private HelloWorld helloWorld;
+
+    @Before
+    public void setup() {
+        helloWorld = new HelloWorld();
+    }
 
     @Test
     public void getMessage_should_return_helloWorld() {
-        HelloWorld helloWorld = new HelloWorld();
         assertEquals("Hello World ", helloWorld.getMessage(""));
     }
 
     @Test
     public void getMessage_should_return_helloWorld_with_passed_name() {
-        HelloWorld helloWorld = new HelloWorld();
         assertEquals("Hello World Anand", helloWorld.getMessage("Anand"));
     }
 
